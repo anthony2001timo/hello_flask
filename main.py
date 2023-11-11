32edef3dbde692cb97fef87b9aad43070a5f7ab7
anthony2001timo <132690955+anthony2001timo@users.noreply.github.com>
2023-11-11 18:55:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return'Hellow, world'
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
+@app.route("/username/<name>/<int:number>")
+def greet(name,number):
+    return f"Hello {name}, you are {number} years old!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+greet()
